f0b63a20cd953e46f78cf4e70c0aded884268da6
Yuheng Chen <yuhengchen@sensetime.com>
2017-03-10 14:05:46 +0800

    Add LengthFieldBasedFrameDecoder

---
+++ src/LengthFieldBasedFrameDecoder.cpp
@@ -0,0 +1,119 @@
+/*
+ *  Copyright (c) 2017, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include <futures/codec/LengthFieldBasedFrameDecoder.h>
+#include "folly/GlogDummy.h"
+
+using folly::IOBuf;
+using folly::IOBufQueue;
+
+namespace futures {
+namespace codec {
+
+LengthFieldBasedFrameDecoder::LengthFieldBasedFrameDecoder(
+  uint32_t lengthFieldLength,
+  uint32_t maxFrameLength,
+  uint32_t lengthFieldOffset,
+  int32_t lengthAdjustment,
+  uint32_t initialBytesToStrip,
+  bool networkByteOrder)
+    : lengthFieldLength_(lengthFieldLength)
+    , maxFrameLength_(maxFrameLength)
+    , lengthFieldOffset_(lengthFieldOffset)
+    , lengthAdjustment_(lengthAdjustment)
+    , initialBytesToStrip_(initialBytesToStrip)
+    , networkByteOrder_(networkByteOrder)
+    , lengthFieldEndOffset_(lengthFieldOffset + lengthFieldLength) {
+  CHECK(maxFrameLength > 0);
+  CHECK(lengthFieldOffset <= maxFrameLength - lengthFieldLength);
+}
+
+Optional<LengthFieldOut> LengthFieldBasedFrameDecoder::decode(folly::IOBufQueue& buf) {
+  // discarding too long frame
+  if (buf.chainLength() < lengthFieldEndOffset_) {
+    return none;
+  }
+
+  uint64_t frameLength = getUnadjustedFrameLength(
+    buf, lengthFieldOffset_, lengthFieldLength_, networkByteOrder_);
+
+  frameLength += lengthAdjustment_ + lengthFieldEndOffset_;
+
+  if (frameLength < lengthFieldEndOffset_) {
+    buf.trimStart(lengthFieldEndOffset_);
+    throw std::runtime_error("Frame too small");
+  }
+
+  if (frameLength > maxFrameLength_) {
+    buf.trimStart(frameLength);
+    throw std::runtime_error("Frame larger than " + std::to_string(maxFrameLength_));
+  }
+
+  if (buf.chainLength() < frameLength) {
+    return none;
+  }
+
+  if (initialBytesToStrip_ > frameLength) {
+    buf.trimStart(frameLength);
+    throw std::runtime_error("InitialBytesToSkip larger than frame");
+  }
+
+  buf.trimStart(initialBytesToStrip_);
+  int actualFrameLength = frameLength - initialBytesToStrip_;
+  return Out(buf.split(actualFrameLength));
+}
+
+uint64_t LengthFieldBasedFrameDecoder::getUnadjustedFrameLength(
+  IOBufQueue& buf, int offset, int length, bool networkByteOrder) {
+  folly::io::Cursor c(buf.front());
+  uint64_t frameLength;
+
+  c.skip(offset);
+
+  switch(length) {
+    case 1:{
+      if (networkByteOrder) {
+        frameLength = c.readBE<uint8_t>();
+      } else {
+        frameLength = c.readLE<uint8_t>();
+      }
+      break;
+    }
+    case 2:{
+      if (networkByteOrder) {
+        frameLength = c.readBE<uint16_t>();
+      } else {
+        frameLength = c.readLE<uint16_t>();
+      }
+      break;
+    }
+    case 4:{
+      if (networkByteOrder) {
+        frameLength = c.readBE<uint32_t>();
+      } else {
+        frameLength = c.readLE<uint32_t>();
+      }
+      break;
+    }
+    case 8:{
+      if (networkByteOrder) {
+        frameLength = c.readBE<uint64_t>();
+      } else {
+        frameLength = c.readLE<uint64_t>();
+      }
+      break;
+    }
+  }
+
+  return frameLength;
+}
+
+}
+} // namespace wangle
